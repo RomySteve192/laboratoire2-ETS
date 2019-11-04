b100753eec765a961a57d219b968c7c455fa6097
Romy Steve <romysteve192@gmail.com>
2019-11-04 01:21:40 -0500

    framework

---
+++ src/laboratoire2/log121/Laboratoire2LOG121.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratoire2.log121;
+
+/**
+ *
+ * @author Romy Steve
+ */
+public class Laboratoire2LOG121 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
