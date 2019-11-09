7d82daa15b933a1901c5b5178919ceca765a9a40
vaudranpc <anhepascal@gmail.com>
2019-11-08 19:11:56 -0500

    Update StrategieBuncoPlusTest.java

--- src/Tests/StrategieBuncoPlusTest.java
+++ src/Tests/StrategieBuncoPlusTest.java
@@ -1,5 +1,15 @@
 package Tests;
 
+import BuncoPlus.StrategieJeuBancoPlus;
+import DiceFramework.Jeu;
+
 public class StrategieBuncoPlusTest {
 
+	
+	public void calculerScoreTourTest() {
+
+		StrategieJeuBancoPlus sjbp = new  StrategieJeuBancoPlus();
+		Jeu j = new Jeu();
+		sjbp.calculerScoreTour();
+	}
 }
